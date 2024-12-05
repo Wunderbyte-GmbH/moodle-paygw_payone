ca33a03398cc19a166d959532940ccb4fd1b60ca
Bernhard Fischer <bernhard.johannes.fischer@gmail.com>
2024-12-05 09:58:56 +0100

    Version 0.2.12 (2024120500)

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release = 'v0.2.11';
-$plugin->version   = 2024120400;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->release = 'v0.2.12';
+$plugin->version   = 2024120500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051100;        // Requires this Moodle version.
 $plugin->component = 'paygw_payone';       // Full name of the plugin (used for diagnostics).
